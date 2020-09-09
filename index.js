07befc1ab29a29ee6a382baa3918a3d4190f7144
lixu <7731204+lixu990213@user.noreply.gitee.com>
2020-09-09 13:52:04 +0800

    “首次提交”

---
+++ index.js
@@ -0,0 +1,16 @@
+const express =require('express');
+const pool = require('./pool');
+
+let port = 5050;   //新浪云中的Node.js只允许5050
+let app = express();
+app.listen(port,()=>{
+    console.log('Server Listening: ',port )
+});
+
+app.get ('/user',(req,res)=>{
+    pool.query('SELECT * FROM xz_user',(err,result)=>{
+        if(err) throw err;
+        res.set('Access-Control-Allow-Origin','*');
+        res.json(result);
+    })
+})
